30e3bcf4eda0ce2241cdd2e62354653b9d0177d1
Thuy Vu <thuy.vu@8seneca.com>
2026-07-24 14:50:53 +0700

    feat: setup Hilt dependency injection close #6

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,8 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
+    alias(libs.plugins.hilt)
+    alias(libs.plugins.ksp)
 }
 
 android {
@@ -57,4 +59,6 @@ dependencies {
     androidTestImplementation(libs.androidx.compose.ui.test.junit4)
     debugImplementation(libs.androidx.compose.ui.tooling)
     debugImplementation(libs.androidx.compose.ui.test.manifest)
+    implementation(libs.hilt.android)
+    ksp(libs.hilt.compiler)
 }
---
+++ app/src/main/java/com/dearstranger/DearStrangerApp.kt
@@ -0,0 +1,8 @@
+package com.dearstranger
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class DearStrangerApp : Application(){
+}
